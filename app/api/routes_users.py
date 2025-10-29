a7bc86fbdcd8ffcb55f6b0062fa730a5037d15cb
Bastian <bastianhhb@icloud.com>
2025-10-29 19:09:31 +0100

    Authentication, and config file

--- app/api/routes_users.py
+++ app/api/routes_users.py
@@ -1,7 +1,38 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from app.core.auth import hash_password, verify_password_hash, create_access_token, verify_token
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from app.core.database import get_db
+from app.models.db_models import User
 
 router = APIRouter()
 
-@router.get("/")
-def list_users():
-    return {"id": 1, "name": "Bob"}
+def check_email(email: str, db: Session = Depends(get_db)):
+    if db.execute(select(User).filter(User.email == email)).scalar():
+        return True
+    else:
+        return False
+
+
+
+@router.post("/register")
+def register(email: str, username: str, password: str, db: Session = Depends(get_db)):
+    if check_email(email):
+        raise HTTPException(status_code=400, detail="Already an account with this email")
+    hashed_password = hash_password(password)
+    new_user = User(
+        email=email,
+        password=hashed_password,
+        username=username,
+    )
+    db.add(new_user)
+    db.commit()
+    return {"msg": "user created"}
+
+@router.post("/login")
+def login(email: str, password: str, db: Session = Depends(get_db)):
+    user = db.execute(select(User).filter(User.email == email)).scalar()
+    if not user or not verify_password_hash(password, user.password):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    token = create_access_token({"sub": email})
+    return {"access_token": token, "token_type": "bearer"}
